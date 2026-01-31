6ddc070236b735ef688f14a9a2e1f6551448bcc6
KOIZUMI Satoru <koizumistr@minos.ocn.ne.jp>
2026-01-31 15:14:01 +0900

    Rubocop

--- cdxview.rb
+++ cdxview.rb
@@ -2,15 +2,14 @@ require 'json'
 require 'csv'
 
 if ARGV[0].nil?
-  puts "Usage: ruby cdxview.rb <cyclonedx_json_file>"
+  puts 'Usage: ruby cdxview.rb <cyclonedx_json_file>'
   exit 1
 end
 
 json_data = File.read(ARGV[0])
 data = JSON.parse(json_data)
 
-
-headers = ['PackageFileName', 'PackageVersion',  'license']
+headers = %w[PackageFileName PackageVersion license]
 
 result = CSV.generate do |csv|
   csv << headers
@@ -20,19 +19,15 @@ result = CSV.generate do |csv|
     version = c['version']
     license_str = 'NOASSERTION'
 
-    if not c['licenses'].nil? then
-      STDERR.puts "warning: multiple liceneses for #{name}" if c['licenses'].size > 1
+    unless c['licenses'].nil?
+      warn("warning: multiple liceneses for #{name}") if c['licenses'].size > 1
 
       licenses0 = c['licenses'][0]
       license_str = licenses0['expression']
-      if not licenses0['license'].nil? then
+      unless licenses0['license'].nil?
         license_str = licenses0['license']['id'] || licenses0['license']['name']
-        if licenses0['license']['name'] == 'Unknown - See URL' then
-          if not licenses0['license']['url'].nil? then
-            license_str = licenses0['license']['url']
-          else
-            license_str = c['externalReferences'][0]['url']
-          end
+        if licenses0['license']['name'] == 'Unknown - See URL'
+          license_str = licenses0['license']['url'] || c['externalReferences'][0]['url']
         end
       end
     end
